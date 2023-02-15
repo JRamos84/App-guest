39f868650efcfad52f601d10ee8acd140c4dc0d7
José <joseph0001@gmail.com>
2023-02-15 16:23:22 +0000

    quedamos en el minuto 54:47

--- App.js
+++ App.js
@@ -1,2 +1,13 @@
+//@ts-check
+
 import { questions } from "./data/questions.js";
-console.log(questions);
+import { Quiz } from "./models/Quiz.js";
+import {UI} from "./models/UI.js";
+
+function main() {
+  const quiz = new Quiz(questions);
+  const ui = new UI();
+  ui.showQuestion('esta es la pregunta')
+}
+
+main();
---
+++ models/UI.js
@@ -0,0 +1,13 @@
+export class UI {
+    constructor(){}
+
+    /**
+     *  
+     * @param {string} text  question to render
+     */
+    showQuestion(text){
+        const questionTitle = document.getElementById('question');
+        console.log(questionTitle)
+    }
+
+}
