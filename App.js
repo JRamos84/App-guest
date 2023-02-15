e7db5b3c68c24073f44c7e5305ed447cd1a80c2d
José Ramos <joseph0001@gmail.com>
2023-02-14 22:27:08 -0300

    Listo parte UI y logica inicial de la App

---
+++ App.js
@@ -0,0 +1,2 @@
+import { questions } from "./data/questions.js";
+console.log(questions)
---
+++ data/data.js
@@ -0,0 +1,28 @@
+
+export const data =
+
+[
+{
+    question:'Cual de las siguientes opciones no es un editor de codigo',
+
+    choices:['vim','vscode','emacs','word'],
+    answer: 'word'
+},
+{
+    question:'Que lenguaje no es orientada a objeto',
+    choices:['java','python','c++','haskell'],
+    answer:'haskell'
+},
+{
+    question:'Que lenguaje  no sirve para sitios web',
+    choices:[
+        'stylus',
+        'less',
+        'sass',
+        'nNxtCss',
+        'PostCss'
+    ],
+    answer:'PostCss'
+}
+
+];
